5c1facc1ee1f142e6796bf393e5a6f36ce8186a0
ashutosh_m <ashutosh_m@excellencetechnologies.in>
2017-10-24 11:08:49 +0530

    get one task api and other updates

--- routes/index.js
+++ routes/index.js
@@ -188,7 +188,6 @@ router.post('/share_task', function(req, res, next) {
                 } else if (users_data) {
                     var users_id = [];
                     req.get_task.find({ _id: req.body._id }, function(err, data) {
-
                         users_id = data[0].users_id;
                         users_id.push({
                             id: users_data._id
@@ -217,5 +216,22 @@ router.post('/share_task', function(req, res, next) {
         }
     })
 });
+
+router.get('/get_one_task', function(req, res, next) {
+    validation.validateAccess(req, function(err, access_token_data) {
+        if (err) {
+            next({ error: err, status: 403 });
+        } else {
+            req.get_task.find({ $and: [{ _id: req.headers.task_id }, { users_id: { "$elemMatch": { "id": access_token_data.user_id } } }] }).exec(function(err, data) {
+                if (err) {
+                    next(err);
+                } else if (data) {
+                    res.json({ error: 0, message: "task", data: data });
+                }
+            })
+        }
+    });
+
+})
 
 module.exports = router;
